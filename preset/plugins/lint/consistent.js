0eca10fb79b32a54e210003c27812d63d654223f
Whizark <contact@whizark.com>
2016-10-23 05:13:35 +0900

    simplify plugin name key

--- preset/plugins/lint/consistent.js
+++ preset/plugins/lint/consistent.js
@@ -1,6 +1,6 @@
 module.exports = {
     "plugins": {
-        "remark-lint": {
+        "lint": {
             "externals"                        : [],
             "blockquote-indentation"           : "consistent",
             "checkbox-character-style"         : "consistent",

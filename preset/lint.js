6471c98d2c11e9017ae2f3a70566ab225f78b49c
Whizark <contact@whizark.com>
2016-12-25 04:09:58 +0900

    add `index.js` for remark-lint settings

--- preset/lint.js
+++ preset/lint.js
@@ -1,3 +1,11 @@
 'use strict';
 
-module.exports = require('./lint-consistent.js');
+module.exports = {
+    "output" : false,
+    "presets": [
+        "./settings/common.js",
+        "./settings/parse.js",
+        "./settings/stringify.js",
+        "./plugins/lint",
+    ].map(require.resolve),
+};
